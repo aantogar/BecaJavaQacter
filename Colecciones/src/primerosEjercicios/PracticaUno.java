ebbc0fbd6f82e035ee27657632b57aca15cc9d27
aantogar <andrea.anton.garcia@accenture.com>
2023-01-19 18:01:18 +0100

    actualización

--- Colecciones/src/primerosEjercicios/PracticaUno.java
+++ Colecciones/src/primerosEjercicios/PracticaUno.java
@@ -9,23 +9,39 @@ public class PracticaUno {
 	public static void main(String[] args) {
 /**
  * Primera practica de colecciones
+ * viendo formas de hacerlo
  */
 		// Declaración el ArrayList
-		List<String> nombreArrayList = new ArrayList<String>();
+		List<String>  coches= new ArrayList<String>();
 
 		// Añadimos 10 Elementos en el ArrayList
 		for (int i=1; i<=10; i++){
-			nombreArrayList.add("Elemento "+i); 
+			coches.add("coche "+i); 
 		}
 
 		// Añadimos un nuevo elemento al ArrayList en la posición 2
-		nombreArrayList.add(2, "Elemento 3");
+		coches.add(2, "coche 3");
 
 		// Declaramos el Iterador e imprimimos los Elementos del ArrayList
-		Iterator<String> nombreIterator = nombreArrayList.iterator();
+		Iterator<String> nombreIterator = coches.iterator();
 		while(nombreIterator.hasNext()){
 			String elemento = nombreIterator.next();
 			System.out.print(elemento+" / ");
+	
+			}
+		String busqueda = "coche 1";
+		boolean existe = coches.contains(busqueda);
+		if (existe) {
+			System.out.println("\nEl elemento SÍ existe en la lista");
+		} else {
+			System.out.println("\nEl elemento no existe");
+		}
+		String busqueda1 = "coche 27";
+		int indice = coches.indexOf(busqueda1);
+		if (indice != -1) {
+			System.out.println("La búsqueda está en el índice " + indice);
+		} else {
+			System.out.println("El elemento no existe");
 		}
 
 	}
