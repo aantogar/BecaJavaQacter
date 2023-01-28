55d6004c741a36a62e349a56f7fcf3c55ccb554e
aantogar <andrea.anton.garcia@accenture.com>
2023-01-28 14:31:45 +0100

    Carpetas organizadas

---
+++ CursoRevisado/src/ejerciciosRevisadosPrimerosdias/FactorialRecursivo.java
@@ -0,0 +1,20 @@
+package ejerciciosRevisadosPrimerosdias;
+
+public class FactorialRecursivo {
+	public static void main(String[]args) {
+		/**
+		 * Realizamos función para resolver factorial
+		 */
+		int fact=8;
+		long factorial;
+		factorial=1;
+		for(int i=fact; i>0; i--) {
+			factorial=factorial*i;
+		}
+		System.out.println("Ejercicio de factorial:\n");
+		System.out.println("El factorial del num "+fact+
+				" es: "+factorial);
+		
+	}
+
+}
---
+++ CursoRevisado/src/ejerciciosRevisadosPrimerosdias/NumerosPrimos.java
@@ -0,0 +1,44 @@
+package ejerciciosRevisadosPrimerosdias;
+
+public class NumerosPrimos {
+	static boolean primo;
+
+	public static void main(String[] args) {
+		/**
+		 * Declaramos el ejercicio y 
+		 * llamamos a su método correspondiente
+		 */
+		 
+		 	System.out.println("EJERCICIO DE NÚMEROS PRIMOS\n");
+	        System.out.println("Son números primos del 1 al 100:");
+	        comprobarPrimos();
+
+	    }
+	public static void comprobarPrimos() {
+	      /**
+	       * Función realizada con un for y luego
+	       *  comprobamos con un if los que son primos para agregar contador
+	       */
+	        int contador=0;
+	        for (int num = 1; num <= 100; num++) {
+	            primo = true;
+	            for (int i = num - 1; i > 1; i--) {
+	                if (num % i == 0) {
+	                    primo = false;
+	                    break;
+	                }
+	            }
+	            if (primo) {
+	            	contador++;
+	                System.out.println(num + " es primo");
+	            }
+	        }
+	        System.out.println("El total de números primos es: "+contador);
+	}
+		
+}
+
+
+	
+
+
