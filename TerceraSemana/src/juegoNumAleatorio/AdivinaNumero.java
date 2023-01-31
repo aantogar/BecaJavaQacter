23d5b549b415a78386296d762d3446241a264877
aantogar <andrea.anton.garcia@accenture.com>
2023-01-31 14:39:29 +0100

    Juego adivinar organizado y modificado

---
+++ TerceraSemana/src/juegoNumAleatorio/AdivinaNumero.java
@@ -0,0 +1,38 @@
+package juegoNumAleatorio;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class AdivinaNumero {
+     
+	public void Adivina()
+	{     
+	int numero;
+	int recibido;
+	Random aleatorio = new Random();
+	Scanner entrada = new Scanner(System.in);
+	 
+	numero = aleatorio.nextInt();
+	 
+	System.out.printf("\nTengo un numero ");
+	System.out.println("Puede adivinarlo?");
+	System.out.println("\nIntroduce número: \n");
+	recibido = entrada.nextInt();
+	while ( recibido != numero )
+	{   
+	if ( recibido > numero )
+	{ 
+	System.out.println("\nEl numero es menor.");
+	recibido = entrada.nextInt();
+	}   else{ 
+	 System.out.println("\nEl es mayor.");
+	 recibido = entrada.nextInt();
+	}  
+	}   
+
+	System.out.println("\nHAS ADIVINADO EL NUMERO!");
+
+	}           
+	
+
+}
--- TerceraSemana/src/juegoNumAleatorio/JuegoContraMaquina.java
+++ TerceraSemana/src/juegoNumAleatorio/JuegoContraMaquina.java
@@ -6,31 +6,13 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class JuegoContraMaquina {
-	private static int mio;
-	private static int robot;
-	 public static void main(String[] args) {
-		 Scanner teclado = new Scanner(System.in);
+	public static void main (String[]args) {
 			
-			System.out.println("ESTO ES UN JUEGO\n Introduce número: ");
-			mio = teclado.nextInt(); 
-			teclado.nextLine();
-			robot=(int)(Math.random()*1000)+1;
-			do {
-				if(mio<robot) {
-					System.out.println("Mi número es mayor");
-				}
-				
-				else {
-					System.out.println("Mi número es menor");
-
-				}
-			}
-			while(mio!=robot);
-			System.out.println("¡LOS NÚMEROS SON IGUALES!: "+robot);
-	    
-	        
-	 }
+			System.out.println("ESTO ES UN JUEGO de adivinar el número.\n  ");
+			AdivinaNumero ad=new AdivinaNumero();
+			ad.Adivina();
 	 
+	}
 }
 
 
