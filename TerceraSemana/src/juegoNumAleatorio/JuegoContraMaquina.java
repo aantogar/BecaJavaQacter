86ba9c01c1b9ab36602a341619042564fbcef30b
aantogar <andrea.anton.garcia@accenture.com>
2023-01-31 14:20:59 +0100

    Juego robot

--- TerceraSemana/src/juegoNumAleatorio/JuegoContraMaquina.java
+++ TerceraSemana/src/juegoNumAleatorio/JuegoContraMaquina.java
@@ -6,51 +6,34 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class JuegoContraMaquina {
-	private static String mayor="m";
-	private static String menor="n";
-	private static String f="f";
-	private static String respuesta="";
+	private static int mio;
+	private static int robot;
 	 public static void main(String[] args) {
-	        // Generar número aleatorio entre 1 y 100
-		  	Random r = new Random();
-		  	int valorX= r.nextInt(100)+1;
-		  	int numeroElegido;
-		  	int maquina;
-		  	//instanciamos el scanner
-	        Scanner sc = new Scanner(System.in);
-	        //Enunciado para el juego
-	        System.out.println("Esto es un JUEGO\n ");
-	        //Comienza el bucle hasta que se acierte
-	        do {
-	            System.out.println("Piensa un número");
-	            System.out.println("Es mayor o es menor ? el de la máquina ");
-	            maquina=valorX;
-	            numeroElegido = sc.nextInt();
-	            Scanner entrada=new Scanner(System.in);
-	            respuesta=entrada.nextLine();
-	            if(respuesta.equalsIgnoreCase("m")) {
-	            	if(maquina<numeroElegido) {
-	            		System.out.println("No es menor");
-	            		System.out.println(maquina);
-	            	}
-	            }else if(respuesta.equalsIgnoreCase("n")) {
-	            	if(maquina>numeroElegido) {
-	            		System.out.println("No es mayor");
-	            		System.out.println(maquina);
-	            	}
-	            	else if(respuesta.equalsIgnoreCase("f")) {
-		            	if(maquina==numeroElegido) {
-		            		System.out.println("Nada que no ganaste!!");
-		            		System.out.println(maquina);
-		            	}
-	            }
-	            }
-	        } while (maquina != numeroElegido);
-	        // Si termina el ciclo while es porque los números son iguales. Felicitamos al usuario
-	        System.out.println("HAS GANADO!!!");
+		 Scanner teclado = new Scanner(System.in);
+			
+			System.out.println("ESTO ES UN JUEGO\n Introduce número: ");
+			mio = teclado.nextInt(); 
+			teclado.nextLine();
+			robot=(int)(Math.random()*1000)+1;
+			do {
+			if(mio<robot) {
+				System.out.println("el número es mayor");
+				System.out.println("Vuelve a intentarlo: ");
+				mio = teclado.nextInt(); 
+				teclado.nextLine();
+			}
+			else {
+				System.out.println("el número es menor");
+				System.out.println("Vuelve a intentarlo: ");
+				mio = teclado.nextInt(); 
+				teclado.nextLine();
+			}
+			}
+			while(mio!=robot);
+			System.out.println("¡HAS GANADO AL ROBOT!: "+robot);
 	    
-	        }
-	
+	        
+	 }
 	 
 }
 
